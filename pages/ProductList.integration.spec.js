bc2e2bbd01e6b76836db8b327ca5a4d2ab5f1ef8
Italo Gois <italo.alberto26@gmail.com>
2022-04-16 16:50:34 -0300

    testes integracao lista de produtos

--- pages/ProductList.integration.spec.js
+++ pages/ProductList.integration.spec.js
@@ -67,4 +67,28 @@ describe('ProductList - integration', () => {
 
     expect(wrapper.text()).toContain('Problemas ao carregar a lista!')
   })
+
+  it('should be filter product when searched', async () => {
+    const products = [
+      ...server.createList('product', 10),
+      server.create('product', { title: 'Meu relógio amado' }),
+    ]
+    axios.get.mockReturnValue(Promise.resolve({ data: { products } }))
+
+    const wrapper = mount(ProductList, {
+      mocks: {
+        $axios: axios,
+      },
+    })
+
+    await Vue.nextTick()
+
+    const search = wrapper.findComponent(Search)
+    search.find('input[type="search"]').setValue('relógio')
+    await search.find('form').trigger('submit')
+
+    const cards = wrapper.findAllComponents(ProductCard)
+    expect(wrapper.vm.searchTerm).toEqual('relógio')
+    expect(cards).toHaveLength(1)
+  })
 })
